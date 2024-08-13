f674baa61278d6936c8c69d3f685eeb0b894a1a4
PaulinaBach <pybc15@gmail.com>
2024-08-13 00:30:51 -0300

    solucion div

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 document.getElementById("SaludoDelDiv").addEventListener("click", ElDivSaluda);
 
+document.getElementById("boton").addEventListener('click', function(event) {
+    event.stopPropagation(); // Detener la propagación del evento
+    alert('Hola!');
+});
+
 function ElDivSaluda(){
     alert('Hola! Soy el div');
 }
