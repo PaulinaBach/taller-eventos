25f9c3cb4d40aa674144ea8966dd2b7936d51fce
PaulinaBach <pybc15@gmail.com>
2024-08-13 00:14:44 -0300

    agregue comentario

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
+//el document es el html, getelementbyid es el elemento que tiene un id al que le quiero obtener, 
+//addeventlistener es a ese elemento que obtuve quiero agregarle un evento, 
+//entre parentesis pongo como primero argumento el disparador, es decir lo que el usuario hace, 
+//y el 2do argumento es lo que se ejecuta cuando el usuario realiza algo
+
 document.getElementById("SaludoDelDiv").addEventListener("click", ElDivSaluda);
 
 function ElDivSaluda(){
